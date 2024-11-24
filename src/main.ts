f2bdcde14e13e92772d146e8649d6be82160f93d
daylerjeff199906 <daylersan@gmail.com>
2024-11-24 12:18:07 -0500

    fix: change cors

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,9 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const port = process.env.PORT || 3000;
+  app.enableCors({
+    origin: '*',
+  });
   await app.listen(port);
   console.log(`Aplicación corriendo en http://localhost:${port}`);
 }
